43718c1141ed6fc472b0fd019f95605abedb5c55
Chris Iba <ibafeva@gmail.com>
2017-07-24 12:26:40 -0700

    Linked html

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import webapp2
+import os
+import jinja2
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template("templates/home.html")
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
