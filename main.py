f4d08448b03246bc67bbeb332585e71e7228c865
Nicaraguan Nightmare <leojlindo@gmail.com>
2017-07-24 17:46:46 -0700

    more class stuff

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ import webapp2
 import os
 import jinja2
 
+from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-# class Search(ndb.model):
 #
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template("templates/home.html")
-        self.response.write(template.render())
+# class Search(ndb.Model):
+#     artist = ndb.StringProperty()
+#     genres = ndb.StringProperty()
+#     region = ndb.StringProperty()
+#
+# class MainHandler(webapp2.RequestHandler):
+#     def get(self):
+#         template = jinja_environment.get_template("templates/home.html")
+#
+#         template_vars = {
+#             'artist': artist,
+#             'genres': genres,
+#             'region': regions
+#         }
+#         self.response.write(template.render(template_vars))
 #
 # class FilterHandler(webapp2.RequestHandler):
 #     def get(self):
