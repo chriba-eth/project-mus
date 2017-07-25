1b376f0a31c322023152598e07f012824d65fe89
Chris Iba <ibafeva@gmail.com>
2017-07-25 12:22:07 -0700

    Dropdown Bar

--- main.py
+++ main.py
@@ -7,31 +7,21 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class Search(ndb.Model):
-#    artist = ndb.StringProperty()
-    genres = ndb.StringProperty()
-    region = ndb.StringProperty()
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        search = Search.query().fetch()
-        template_vars = {
-            'search': search
-        }
         template = jinja_environment.get_template("templates/home.html")
-        self.response.write(template.render(template_vars))
+        self.response.write(template.render())
 
     def post(self):
-        genres = self.request.get('genres')
-        region = self.request.get('region')
-        results_vars = {
-            'genres': genres,
-            'region': region
-
+        result_vars = {
+            'genres' :self.request.get('genres'),
+            'region' : self.request.get('region')
         }
-        search = Search(genres = genres, region = region)
-        region.put()
-        self.redirect('/')
+        template = jinja_environment.get_template("templates/results.html")
+        self.response.write(template.render(result_vars))
+
 #
 #
 # class FilterHandler(webapp2.RequestHandler):
