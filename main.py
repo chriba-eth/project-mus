b42af2700cd5cfc8a0980ee8ac37df34211544c2
Chris Iba <ibafeva@gmail.com>
2017-07-26 12:28:04 -0700

    Fixed login page

--- main.py
+++ main.py
@@ -30,7 +30,18 @@ jinja_environment = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/home.html")
-        self.response.write(template.render())
+
+        current_user= users.get_current_user()
+        logout_url = users.create_logout_url('/')
+        login_url= users.create_login_url('/')
+
+
+        template_vars = {
+        'current_user':current_user,
+        'logout_url': logout_url,
+        'login_url': login_url,
+        }
+        self.response.write(template.render(template_vars))
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
